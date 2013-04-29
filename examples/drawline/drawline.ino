c4434f2325ed20ce20eaa7a665250a7a381791df
ladyada <limor@ladyada.net>
2013-04-29 09:31:59 -0400

    gfx test

--- examples/drawline/drawline.ino
+++ examples/drawline/drawline.ino
@@ -1,37 +1,184 @@
 /* Draws a line across the screen */
 
-#include <SharpMem.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
 
-#define SCK 2
-#define MOSI 3
-#define SS 4
-#define EXTIN 6
-#define DISPEN 7
+#define SCK 13
+#define MOSI 11
+#define SS 10
+#define EXTIN 9
+#define DISPEN 8
 
-SharpMem sharpmem(SCK, MOSI, SS, EXTIN, DISPEN);
+Adafruit_SharpMem display(SCK, MOSI, SS, EXTIN, DISPEN);
+
+#define BLACK false
+#define WHITE true
 
 void setup(void) 
 {
   Serial.begin(9600);
   Serial.println("Hello!");
 
-  sharpmem.begin();
-  sharpmem.ClearScreen();
+  display.begin();
+  display.clearDisplay();
+  // Turn the screen on  
+  display.enable(true);
   
   uint32_t i;
   for (i = 0; i < 96; i++)
   {
-    sharpmem.DrawPixel(i, i);
+    display.drawPixel(i, 96-i, BLACK);
   }
 
-  // Turn the screen on  
-  sharpmem.Enable(true);
+    // draw a single pixel
+  display.drawPixel(10, 10, BLACK);
+  display.refresh();
+  delay(500);
+  display.clearDisplay();
+
+  // draw many lines
+  testdrawline();
+  /*
+  display.refresh();
+  delay(2000);
+  display.clearDisplay();
+
+  // draw rectangles
+  testdrawrect();
+  display.refresh();
+  delay(2000);
+  display.clearDisplay();
+  */
+  
 }
 
 void loop(void) 
 {
   // Screen must be refreshed at least once per second
-  sharpmem.Refresh();
+  display.refresh();
   delay(500);
 }
 
+///
+
+
+void testdrawchar(void) {
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(0,0);
+
+  for (uint8_t i=0; i < 168; i++) {
+    if (i == '\n') continue;
+    display.write(i);
+    //if ((i > 0) && (i % 14 == 0))
+      //display.println();
+  }    
+  display.refresh();
+}
+
+void testdrawcircle(void) {
+  for (uint8_t i=0; i<display.height(); i+=2) {
+    display.drawCircle(display.width()/2, display.height()/2, i, BLACK);
+    display.refresh();
+  }
+}
+
+void testfillrect(void) {
+  uint8_t color = 1;
+  for (uint8_t i=0; i<display.height()/2; i+=3) {
+    // alternate colors
+    display.fillRect(i, i, display.width()-i*2, display.height()-i*2, color%2);
+    display.refresh();
+    color++;
+  }
+}
+
+void testdrawtriangle(void) {
+  for (uint16_t i=0; i<min(display.width(),display.height())/2; i+=5) {
+    display.drawTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, BLACK);
+    display.refresh();
+  }
+}
+
+void testfilltriangle(void) {
+  uint8_t color = BLACK;
+  for (int16_t i=min(display.width(),display.height())/2; i>0; i-=5) {
+    display.fillTriangle(display.width()/2, display.height()/2-i,
+                     display.width()/2-i, display.height()/2+i,
+                     display.width()/2+i, display.height()/2+i, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.refresh();
+  }
+}
+
+void testdrawroundrect(void) {
+  for (uint8_t i=0; i<display.height()/2-2; i+=2) {
+    display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, BLACK);
+    display.refresh();
+  }
+}
+
+void testfillroundrect(void) {
+  uint8_t color = BLACK;
+  for (uint8_t i=0; i<display.height()/2-2; i+=2) {
+    display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i, display.height()/4, color);
+    if (color == WHITE) color = BLACK;
+    else color = WHITE;
+    display.refresh();
+  }
+}
+   
+void testdrawrect(void) {
+  for (uint8_t i=0; i<display.height()/2; i+=2) {
+    display.drawRect(i, i, display.width()-2*i, display.height()-2*i, BLACK);
+    display.refresh();
+  }
+}
+
+void testdrawline() {  
+  for (uint8_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, 0, i, display.height()-1, BLACK);
+    display.refresh();
+  }
+  for (uint8_t i=0; i<display.height(); i+=4) {
+    display.drawLine(0, 0, display.width()-1, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (uint8_t i=0; i<display.width(); i+=4) {
+    display.drawLine(0, display.height()-1, i, 0, BLACK);
+    display.refresh();
+  }
+  for (int8_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(0, display.height()-1, display.width()-1, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+  
+  display.clearDisplay();
+  for (int8_t i=display.width()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, i, 0, BLACK);
+    display.refresh();
+  }
+  for (int8_t i=display.height()-1; i>=0; i-=4) {
+    display.drawLine(display.width()-1, display.height()-1, 0, i, BLACK);
+    display.refresh();
+  }
+  delay(250);
+
+  display.clearDisplay();
+  for (uint8_t i=0; i<display.height(); i+=4) {
+    display.drawLine(display.width()-1, 0, 0, i, BLACK);
+    display.refresh();
+  }
+  for (uint8_t i=0; i<display.width(); i+=4) {
+    display.drawLine(display.width()-1, 0, i, display.height()-1, BLACK); 
+    display.refresh();
+  }
+  delay(250);
+}
