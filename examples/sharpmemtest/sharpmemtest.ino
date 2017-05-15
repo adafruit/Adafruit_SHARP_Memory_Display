c43614492ee0da8debf17f65d958aa55904cc81d
ladyada <limor@ladyada.net>
2017-05-15 19:10:09 -0400

    make example not dumb

--- examples/sharpmemtest/sharpmemtest.ino
+++ examples/sharpmemtest/sharpmemtest.ino
@@ -20,11 +20,11 @@ All text above, and the splash screen must be included in any redistribution
 #include <Adafruit_SharpMem.h>
 
 // any pins can be used
-#define SCK 10
-#define MOSI 11
-#define SS 13
+#define SHARP_SCK 13
+#define SHARP_MOSI 11
+#define SHARP_SS 13
 
-Adafruit_SharpMem display(SCK, MOSI, SS);
+Adafruit_SharpMem display(SHARP_SCK, SHARP_MOSI, SHARP_SS);
 
 #define BLACK 0
 #define WHITE 1
