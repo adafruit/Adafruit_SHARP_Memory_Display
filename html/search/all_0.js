6e2449376743046977a2be89232b6dc89bd110d3
Doxygen CI <travis@travis-ci.org>
2020-07-19 01:21:33 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fsharpmem',['Adafruit_SharpMem',['../class_adafruit___sharp_mem.html',1,'Adafruit_SharpMem'],['../class_adafruit___sharp_mem.html#a47972703621ba8f8de88bd6b60dce584',1,'Adafruit_SharpMem::Adafruit_SharpMem()']]]
+  ['adafruit_5fsharpmem',['Adafruit_SharpMem',['../class_adafruit___sharp_mem.html',1,'Adafruit_SharpMem'],['../class_adafruit___sharp_mem.html#a75ef48abd136bdd4239dc6c6c0c23ebb',1,'Adafruit_SharpMem::Adafruit_SharpMem(uint8_t clk, uint8_t mosi, uint8_t cs, uint16_t w=96, uint16_t h=96, uint32_t freq=2000000)'],['../class_adafruit___sharp_mem.html#aff44c86caa414f4120dab37083d6ba5a',1,'Adafruit_SharpMem::Adafruit_SharpMem(SPIClass *theSPI, uint8_t cs, uint16_t w=96, uint16_t h=96, uint32_t freq=2000000)']]]
 ];
