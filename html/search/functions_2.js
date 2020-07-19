cc772e7885055bd021e6c70017a45cea920df25e
Doxygen CI <travis@travis-ci.org>
2020-07-19 01:42:09 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['cleardisplay',['clearDisplay',['../class_adafruit___sharp_mem.html#a1315da3c35840e2d62dea87e33ac7c21',1,'Adafruit_SharpMem']]]
+  ['cleardisplay',['clearDisplay',['../class_adafruit___sharp_mem.html#a1315da3c35840e2d62dea87e33ac7c21',1,'Adafruit_SharpMem']]],
+  ['cleardisplaybuffer',['clearDisplayBuffer',['../class_adafruit___sharp_mem.html#a4731701693c89a1d346305faa0442756',1,'Adafruit_SharpMem']]]
 ];
