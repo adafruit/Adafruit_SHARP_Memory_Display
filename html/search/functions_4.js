2550c61e6a4ff25b268dc5e41ce5ed27210ec4c6
Doxygen CI <ci-arduino@invalid>
2025-03-18 03:52:24 +0000

    Deploy docs to GitHub Pages from commit 72e26b3502
    Commit: 72e26b350227a71acae777cc6bcd2b48f82091e6
    GitHub Actions run: 13914994859

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['getbuffer',['getBuffer',['../class_adafruit___sharp_mem.html#af823aaa30ed4fc67564ccd10dba70279',1,'Adafruit_SharpMem']]],
   ['getpixel',['getPixel',['../class_adafruit___sharp_mem.html#aff699cfe3249d35151966d3999411e1d',1,'Adafruit_SharpMem']]]
 ];
